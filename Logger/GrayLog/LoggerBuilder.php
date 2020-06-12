a5372d8d667ab1865507e11bf4d802f8cadf8729
Hieu Le <hidro@forixwebdesign.com>
2020-06-12 19:24:54 +0700

    Init module

---
+++ Logger/GrayLog/LoggerBuilder.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Created by Hidro Le.
+ * Job Title: Magento Developer
+ * Project Name: M2.3.0 - CE - Default Magento
+ * Date: 10/06/2020
+ * Time: 23:55
+ */
+
+namespace Solutiontutorials\Graylog\Logger\GrayLog;
+
+use Gelf\Publisher;
+use Gelf\Transport\UdpTransport;
+use Gelf\Logger;
+
+class LoggerBuilder
+{
+
+    const XML_GRAYLOG_CONFIG_PREFIX = 'system/graylog/';
+
+    /**
+     * Object manager
+     *
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $_objectManager;
+
+    /**
+     * @var \Gelf\Publisher
+     */
+    protected $_publisher;
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * LoggerBuilder constructor.
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\ObjectManagerInterface          $objectManager
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\UrlInterface $urlBuilder,
+        \Magento\Framework\ObjectManagerInterface $objectManager
+    )
+    {
+        $this->_objectManager = $objectManager;
+        $this->scopeConfig = $scopeConfig;
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    protected function getConfig($path){
+        return $this->scopeConfig->getValue(self::XML_GRAYLOG_CONFIG_PREFIX . $path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isEnabled()
+    {
+        return !!$this->getConfig('enabled');
+    }
+    /**
+     * get config Facility
+     * @return string
+     */
+    protected function getProjectFacility()
+    {
+        $facility = $this->getConfig('facility');
+        $baseUrl = $this->urlBuilder->getBaseUrl();
+        return $facility?:$baseUrl;
+    }
+
+    /**
+     * get config host
+     * @return string
+     */
+    protected function getServerHost(){
+        return $this->getConfig('host');
+    }
+
+    /**
+     * get config port
+     * @return string
+     */
+    protected function getServerPort(){
+        return $this->getConfig('port');
+    }
+
+    protected function getUdpTransport(){
+        $host = $this->getServerHost();
+        $port = $this->getServerPort();
+        if($host && $port) {
+            return $this->_objectManager->create(UdpTransport::class, [
+                'host' => $this->getServerHost(),
+                'port' => $this->getServerPort(),
+                UdpTransport::CHUNK_SIZE_LAN
+            ]);
+        }
+        return null;
+    }
+
+    protected function getPublisher(){
+        if(!$this->_publisher) {
+            $transport = $this->getUdpTransport();
+            if($transport) {
+                /**
+                 * @var $publisher Publisher
+                 */
+                $publisher = $this->_objectManager->get(Publisher::class);
+                $publisher->addTransport($transport);
+                $this->_publisher = $publisher;
+            }
+        }
+        return $this->_publisher;
+    }
+
+    /**
+     * @param $facility
+     * @return Logger
+     */
+    public function prepareHandler($facility = ''){
+        $handler = null;
+        if($this->isEnabled()) {
+            $publisher = $this->getPublisher();
+            if($publisher) {
+                if(!$facility){
+                    $facility = $this->getProjectFacility();
+                }
+                $handler = $this->_objectManager->create(Logger::class, [
+                    'publisher' => $publisher,
+                    'facility' => $facility
+                ]);
+            }
+        }
+        return $handler;
+    }
+}
---
+++ README.md
@@ -0,0 +1,55 @@
+# Mage2 Module Solutiontutorials Graylog
+
+    ``solutiontutorials/module-graylog``
+
+ - [Main Functionalities](#markdown-header-main-functionalities)
+ - [Installation](#markdown-header-installation)
+ - [Configuration](#markdown-header-configuration)
+ - [Specifications](#markdown-header-specifications)
+ - [Attributes](#markdown-header-attributes)
+
+
+## Main Functionalities
+
+
+## Installation
+\* = in production please use the `--keep-generated` option
+
+### Type 1: Zip file
+
+ - Unzip the zip file in `app/code/Solutiontutorials`
+ - Enable the module by running `php bin/magento module:enable Solutiontutorials_Graylog`
+ - Apply database updates by running `php bin/magento setup:upgrade`\*
+ - Flush the cache by running `php bin/magento cache:flush`
+
+### Type 2: Composer
+
+ - Make the module available in a composer repository for example:
+    - private repository `repo.magento.com`
+    - public repository `packagist.org`
+    - public github repository as vcs
+ - Add the composer repository to the configuration by running `composer config repositories.repo.magento.com composer https://repo.magento.com/`
+ - Install the module composer by running `composer require solutiontutorials/module-graylog`
+ - enable the module by running `php bin/magento module:enable Solutiontutorials_Graylog`
+ - apply database updates by running `php bin/magento setup:upgrade`\*
+ - Flush the cache by running `php bin/magento cache:flush`
+
+
+## Configuration
+
+ - Enabled (system/graylog/enabled)
+
+ - Host (system/graylog/host)
+
+ - Port (system/graylog/port)
+
+
+## Specifications
+
+ - Console Command
+	- test
+
+## Attributes
+
+
+
